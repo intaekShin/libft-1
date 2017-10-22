728ff7fb1fe1abdcc5c6d0d4cc101c0a76458a43
davhojt <davhojt@gmail.com>
2017-10-22 23:56:25 +1100

    Updatde libft.h and Makedile with new functions

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 NAME = libft.a
 COMP = gcc -Wall -Werror -Wextra
-CFILE = main.c \
+CFILE = test.c \
+	ft_strlen.c \
 	ft_putchar.c \
 	ft_putstr.c \
 	ft_putnbr.c \
 	ft_atoi.c \
-	ft_strcpy.c
+	ft_strcpy.c \
+	ft_strncpy.c \
+	ft_strstr.c
 
 all:
 	$(COMP) $(CFILE)
--- libft.h
+++ libft.h
@@ -4,7 +4,10 @@
 
 void	ft_putchar(char c);
 void	ft_putstr(char *str);
+int		ft_strlen(char *str);
 void	ft_putnbr(int nb);
 int		ft_atoi(char *str);
 char	*ft_strcpy(char *dest, char *src);
+char	*ft_strncpy(char *dest, char *src, unsigned int n);
+char	*ft_strstr(char *str, char *to_find);
 #endif
