fbfb705b266e31b9f1ee59258a5bdd05dd909cee
davhojt <davhojt@gmail.com>
2017-10-24 21:13:08 +1100

    Completed ft_strncmp

---
+++ ft_strncmp.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+int ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	while ((*s1 && *s2) && n > 0)
+	{
+		if (*s1 == '\0' && *s2 == '\0')
+			return (0);
+		if (*s1 != *s2)
+			return ((int) *s1 - *s2);
+		s1++;
+		s2++;
+		n--;
+	}
+	return (0);
+}
