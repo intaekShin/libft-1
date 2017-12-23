6c00b49bcd401fac4e2884295549e28dd3fdc928
davhojt <davhojt@gmail.com>
2017-12-23 16:28:49 +0800

    Created ft_memset

---
+++ ft_memset.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	char *ptr;
+	size_t i;
+
+	ptr = b;
+	i = 0;
+
+	while (i < len)
+	{
+		ptr[i] = c;
+		i++;
+	}
+	return (b);
+}
