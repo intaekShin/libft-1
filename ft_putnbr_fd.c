bce23d43016a5a92cc4da662e5471a2a662f2fde
davhojt <davhojt@gmail.com>
2017-12-31 17:55:43 +0800

    Modified ft_putnbr_fd to use ft_itoa. Now - ft_putnbr used ft_putnbr_fd and ft_putnbr_fd uses ft_itoa.

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -2,31 +2,5 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	int	temp;
-	int	tens;
-
-	tens = 1;
-
-	if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		if (n == -2147483648)
-		{
-			ft_putchar_fd('2', fd);
-			n = -147483648;
-		};
-		n = -n;
-	}
-
-	temp = n;
-	while ((temp /= 10) > 0)
-		tens *= 10;
-	temp = n;
-
-	while(tens)
-	{
-		ft_putchar_fd((char) (temp / tens) + 48, fd);
-		temp %= tens;
-		tens /= 10;
-	}
+	ft_putstr_fd(ft_itoa(n), fd);
 }
