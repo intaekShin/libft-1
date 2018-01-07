ad68abf6df3a3b724efce308832eaeedaf85e732
davhojt <davhojt@gmail.com>
2018-01-07 23:26:20 +0700

    Shrunk long function down. Needs further work to meet norm

--- ft_lststrsplit.c
+++ ft_lststrsplit.c
@@ -1,60 +1,50 @@
 #include "libft.h"
 
-static t_list	**ft_lstdelim(char *s, char c)
+static t_list	**ft_lstdelim(t_list **head, char *s, char c, int i, int j)
 {
-	t_list	**head;
 	t_list	*lst;
 	t_list	*start;
-	char	*tmp;
-	int		i;
-	int		j;
 	int		first;
 
-	i = 0;
-	j = 0;
 	first = 0;
-	while (s[i] != '\0')
+	while (s[i] != '\0' || s[i - 1] != '\0')
 	{
-		if (!(s[i] == c))
-		{
-			j++;
-			if (s[i + 1] == '\0')
-				i++;
-		}
+		j = (!(s[i] == c)) ? (j += 1) : (j += 0);
+		i = (s[i + 1] == '\0') ? (i += 1) : (i += 0);
 		if (s[i] == c || s[i] == '\0')
 		{
-			tmp = ft_strndup(s + (i - j), j);	
-			if (first == 0)
+			if (first++ == 0)
 			{
-				if(!(lst = ft_lstnew(tmp, j + 1)))
+				if(!(lst = ft_lstnew(ft_strndup(s + (i - j), j), j + 1)))
 					return (NULL);
 				start = lst;
-				first = 1;
 			}
 			else
 			{
-				if(!(lst->next = ft_lstnew(tmp, j + 1)))
+				if(!(lst->next = ft_lstnew(ft_strndup(s + (i - j), j), j + 1)))
 					return (NULL);
 				lst = lst->next;
 			}
 			j = 0;
-			if (s[i] == '\0')
-				i--;
 		}
 		i++;
 	}
-	head = &start;
-	return (head);
+	return (head = &start);
 }
 
 t_list			**ft_lststrsplit(char const *s, char c)
 {	
-	char	*one;
-	char	*two;
-	t_list	**three;
+	char	*trimmed;
+	char	*removed;
+	t_list	**head;
+	int i;
+	int j;
 
-	one = ft_strtrimselect(s, c);
-	two = ft_removeselect(one, c);
-	three = ft_lstdelim(two, c);
-	return (three);
+	i = 0;
+	j = 0;
+	head = NULL;
+	trimmed = ft_strtrimselect(s, c);
+	removed = ft_removeselect(trimmed, c);
+	head = ft_lstdelim(head, removed, c, i, j);
+	return (head);
 }
