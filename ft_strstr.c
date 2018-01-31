e5918a6be5f1d1e79cc1b324b812e05d0f24d324
davhojt <davhojt@gmail.com>
2018-01-31 15:22:54 +0700

    Amended ft_strstr to use ft_strnstr

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/27 15:38:22 by dhojt             #+#    #+#             */
-/*   Updated: 2018/01/27 15:38:24 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/01/31 15:21:24 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,5 @@
 
 char *ft_strstr(const char *haystack, const char *needle)
 {
-	int i;
-	int j;
-
-	i = 0;
-	j = 0;
-	while (*haystack)
-	{
-		if (needle[j] == '\0')
-			return ((char *)&haystack[i - ft_strlen(needle)]);
-		if (haystack[i] == needle[j])
-			j++;
-		else
-			j = 0;
-		i++;
-	}
-	return (NULL);
+	return(ft_strnstr(haystack, needle, ft_strlen(haystack)));
 }
