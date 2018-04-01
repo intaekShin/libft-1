a2c86923f5cd882df36e33af9b48df5b1253e021
davhojt <davhojt@gmail.com>
2018-04-01 21:27:48 +0200

    Modified to allow basic input

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/27 15:36:27 by dhojt             #+#    #+#             */
-/*   Updated: 2018/01/30 22:29:14 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/04/01 21:14:38 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,21 @@
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t i;
 	unsigned char *str_s1;
 	unsigned char *str_s2;
 
-	i = 0;
 	str_s1 = (unsigned char *) s1;
 	str_s2 = (unsigned char *) s2;
 
-	if (!(n))
+	if (!n || !str_s1 || !str_s2)
 		return (0);
-	n--;
-	while(n && *str_s1 && *str_s1 == *str_s2)
+	while(n)
 	{
-		n--;
+		if (*str_s1 != *str_s2)
+			return (*str_s1 - *str_s2);
 		str_s1++;
 		str_s2++;
+		n--;
 	}
-	return ((unsigned char) *str_s1 - (unsigned char) *str_s2);
+	return (0);
 }
