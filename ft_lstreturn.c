92c331218e76ebcb9b3a9c8df4a8293c6536df02
Dav HOJT <dhojt@e2r4p21.42.fr>
2018-04-04 13:57:09 +0200

    Added protection for null pointer. Added protection against n being greater than the number of list elements

--- ft_lstreturn.c
+++ ft_lstreturn.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/27 15:36:14 by dhojt             #+#    #+#             */
-/*   Updated: 2018/01/27 15:36:17 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/04/04 13:55:22 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,11 @@ t_list	*ft_lstreturn(t_list *lst, size_t n)
 	t_list	*tmp;
 	t_list	*ptr;
 
+	if (!lst)
+		return (NULL);
 	i = 0;
 	ptr = lst;
-	while (i < n)
+	while (i < n && tmp)
 	{
 		tmp = ptr->next;
 		ptr = tmp;
