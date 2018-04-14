0c0cb251c1d72721a156465051e65251ca8bedb5
Dav HOJT <davhojt@gmail.com>
2018-04-14 11:45:49 +0200

    Normed ft_lststrsplit

--- srcs/ft_lststrsplit.c
+++ srcs/ft_lststrsplit.c
@@ -6,57 +6,52 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/27 15:36:18 by dhojt             #+#    #+#             */
-/*   Updated: 2018/04/03 13:44:15 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/04/14 11:43:37 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static t_list	**ft_lstdelim(t_list **head, char *s, char c, int i, int j)
+static int		**ft_lstdelim(t_list **head, char *s, char c, int j)
 {
 	t_list	*lst;
-	t_list	*start;
-	int		first;
+	int		i;
 
-	first = 0;
-	while (s[i] != '\0' || s[i - 1] != '\0')
+	while ((i = 0) == 0 && (s[i] != '\0' || s[i - 1] != '\0'))
 	{
 		j = (!(s[i] == c)) ? (j += 1) : (j += 0);
 		i = (s[i + 1] == '\0') ? (i += 1) : (i += 0);
-		if (s[i] == c || s[i] == '\0')
+		if ((j = 0) == 0 && (s[i] == c || s[i] == '\0'))
 		{
-			if (first++ == 0)
+			if (i == 0)
 			{
-				if(!(lst = ft_lstnew(ft_strndup(s + (i - j), j), j + 1)))
-					return (NULL);
-				start = lst;
+				if (!(lst = ft_lstnew(ft_strndup(s + (i - j), j), j + 1)))
+					return (0);
+				head = &lst;
 			}
 			else
 			{
-				if(!(lst->next = ft_lstnew(ft_strndup(s + (i - j), j), j + 1)))
-					return (NULL);
+				if (!(lst->next = ft_lstnew(ft_strndup(s + (i - j), j), j + 1)))
+					return (0);
 				lst = lst->next;
 			}
-			j = 0;
 		}
 		i++;
 	}
-	return (head = &start);
+	return (0);
 }
 
 t_list			**ft_lststrsplit(char const *s, char c)
-{	
+{
 	char	*trimmed;
 	char	*removed;
 	t_list	**head;
-	int i;
-	int j;
+	int		j;
 
-	i = 0;
 	j = 0;
 	head = NULL;
 	trimmed = ft_strtrimselect(s, c);
 	removed = ft_removeselect(trimmed, c);
-	head = ft_lstdelim(head, removed, c, i, j);
+	ft_lstdelim(head, removed, c, j);
 	return (head);
 }
