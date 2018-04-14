a3d838cd50111d8ae97b338ada862ffb6069bf50
Dav HOJT <davhojt@gmail.com>
2018-04-14 11:18:14 +0200

    Modified ft_strjoin to use ft_strnew

--- srcs/ft_strjoin.c
+++ srcs/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/27 15:37:45 by dhojt             #+#    #+#             */
-/*   Updated: 2018/04/03 19:54:32 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/04/12 01:45:10 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,21 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*join_str;
 	size_t	i;
 	size_t	j;
+	size_t	len1;
+	size_t	len2;
 
 	if (s1 && s2)
 	{
-		i = 0;
+		len1 = ft_strlen(s1);
+		len2 = ft_strlen(s2);
+		i = -1;
 		j = 0;
-		join_str = ft_strnew(ft_strlen(s1) + ft_strlen(s2));
-		if (!join_str)
+		if (!(join_str = ft_strnew(len1 + len2)))
 			return (NULL);
-		while (j < ft_strlen(s1))
-			join_str[i++] = s1[j++];
-		j = 0;
-		while (j < ft_strlen(s2))
-		{
-			join_str[i] = s2[j];
-			i++;
-			j++;
-		}
+		while (++i < len1)
+			join_str[i] = s1[i];
+		while (j < len2)
+			join_str[i++] = s2[j++];
 		return (join_str);
 	}
 	return (NULL);
