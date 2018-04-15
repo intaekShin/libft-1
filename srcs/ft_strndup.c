eeadb7d1fb0e98d2c3e16c2c32d17234082a2ff7
Dav HOJT <davhojt@gmail.com>
2018-04-15 21:13:28 +0200

    Reduced seeking operations to speed up tasks.

--- srcs/ft_strndup.c
+++ srcs/ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/27 15:38:01 by dhojt             #+#    #+#             */
-/*   Updated: 2018/01/27 15:38:05 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/04/15 19:00:30 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,15 @@
 char	*ft_strndup(const char *s1, size_t n)
 {
 	char	*s2;
-	size_t	len;
-	size_t	size;
-	int		trail_zero;
+	size_t	i;
 
-	len = ft_strlen(s1);
-	if (len < n)
-		size = len;
-	else
-	{
-		size = n;
-		trail_zero = 1;
-	}
-	s2 = ft_strnew(size);
-	if (!s2)
+	i = 0;
+	if (!(s2 = ft_strnew(n)))
 		return (NULL);
-	ft_strncpy(s2, s1, size);
-	if (trail_zero)
-		s2[size] = '\0';
+	while (s1[i] && i < n)
+	{
+		s2[i] = s1[i];
+		i++;
+	}
 	return (s2);
 }
