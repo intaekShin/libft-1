e6ef2015db86607c6c6f41340ddf09fa361c3b2c
davhojt <davhojt@gmail.com>
2018-04-20 10:27:00 +0200

    Reversing last accidental commit

--- srcs/ft_strchr.c
+++ srcs/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/27 15:37:12 by dhojt             #+#    #+#             */
-/*   Updated: 2018/04/15 19:19:35 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/04/20 10:25:24 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,7 @@ char	*ft_strchr(const char *s, int c)
 			return ((char *)s + i);
 		i++;
 	}
+	if (s[i] == (char)c)
+		return ((char *)s + i);
 	return (NULL);
 }
