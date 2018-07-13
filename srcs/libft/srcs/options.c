8b43c4e273b996658262b5089c43a482e11e724f
davhojt <davhojt@gmail.com>
2018-07-13 16:41:27 +0300

    Developing options

---
+++ srcs/libft/srcs/options.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   options.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/13 15:31:05 by dhojt             #+#    #+#             */
+/*   Updated: 2018/07/13 16:40:58 by dhojt            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//not normed
+#include "libft.h" //do I need this?
+#include <stdio.h> //remove
+
+int					options(char **argv)
+{
+	int				options;
+	char			*str;
+
+	options = 0;
+	argv++;
+	while (*argv)
+	{
+		str = *argv;
+		if (*str == '-' && ft_str_is_alpha(str + 1))
+		{
+			str++;
+			while (*str)
+			{
+				printf("Char: %c\n", *str);
+				str++;
+			}
+		}
+		argv++;
+	}
+	return (0);//
+}
+
+int					main(int argc, char **argv)
+{
+	if (argc && argv)
+		options(argv);
+	return (0);
+}
