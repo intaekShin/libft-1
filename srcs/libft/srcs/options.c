bc438a596ce9c5338cfccc56bda8780f0135e3c7
davhojt <davhojt@gmail.com>
2018-07-13 18:22:57 +0300

    Now set the correct bit

--- srcs/libft/srcs/options.c
+++ srcs/libft/srcs/options.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/13 15:31:05 by dhojt             #+#    #+#             */
-/*   Updated: 2018/07/13 16:51:08 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/07/13 18:16:22 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,21 @@
 #include "libft.h" //do I need this?
 #include <stdio.h> //remove
 
-int					options(char **argv)
+void				treat_options(char c, unsigned long *options)
 {
-	int				options;
+	long			one;
+	one = 1;
+	if (c >= 'a' && c <= 'z')
+		*options |= (one << (c - 97));
+	if (c >= 'A' && c <= 'Z')
+		*options |= (one << (c - 39));
+	if (c >= '0' && c <= '9')
+		*options |= (one << (c + 4));
+}
+
+unsigned long				options(char **argv)
+{
+	unsigned long			options;
 	char			*str;
 
 	options = 0;
@@ -29,18 +41,26 @@ int					options(char **argv)
 			str++;
 			while (*str)
 			{
-				printf("Char: %c\n", *str);
+				treat_options(*str, &options);
 				str++;
 			}
 		}
 		argv++;
 	}
-	return (0);//
+	return (options);//
 }
 
 int					main(int argc, char **argv)
 {
+	unsigned long			result;
+	unsigned long			a;
 	if (argc && argv)
-		options(argv);
+	{
+		result = options(argv);
+		printf("Value %lu\n", result);
+	}
+	a = 0;
+	a |= (1 << 31);
+	printf("THIS %lu\n", a);
 	return (0);
 }
