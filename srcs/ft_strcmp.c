167355126a7ebf0d32ef36ca0463a579f9564ba7
Dav Højt <davhojt@gmail.com>
2019-01-05 15:51:48 +0200

    feat: now only gets len of string once. Don't need to send len of longest string. Just need to compare ar least one character more than at least one string

--- srcs/ft_strcmp.c
+++ srcs/ft_strcmp.c
@@ -14,7 +14,5 @@
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
-	if (ft_strlen(s1) > ft_strlen(s2))
-		return (ft_strncmp(s1, s2, (ft_strlen(s1))));
-	return (ft_strncmp(s1, s2, (ft_strlen(s2))));
+	return (ft_strncmp(s1, s2, (ft_strlen(s1) + 1)));
 }
