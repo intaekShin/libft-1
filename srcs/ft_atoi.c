334f64027eff6e6a0c7c13fa9e27d97b23a877ff
Dav Højt <davhojt@gmail.com>
2019-01-05 14:03:04 +0200

    refactor: atoi now returns atoi max

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -14,21 +14,5 @@
 
 int		ft_atoi(const char *str)
 {
-	int i;
-	int min;
-	int result;
-
-	i = 0;
-	min = 1;
-	result = 0;
-	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\r' || str[i] == '\v'
-			|| str[i] == '\f' || str[i] == ' ')
-		i++;
-	if (str[i] == '-')
-		min = -1;
-	if (str[i] == '-' || str[i] == '+')
-		i++;
-	while (str[i] >= '0' && str[i] <= '9')
-		result = (result * 10) + (str[i++] - '0');
-	return (result *= min);
+	return (ft_atoi_max(str));
 }
