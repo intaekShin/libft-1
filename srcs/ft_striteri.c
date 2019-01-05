cd9299c64b3f91d80ed1217f8ed09a84da9387b9
Dav Højt <davhojt@gmail.com>
2019-01-05 16:01:00 +0200

    refactor: only use i as index to send to function

--- srcs/ft_striteri.c
+++ srcs/ft_striteri.c
@@ -19,10 +19,7 @@ void	ft_striteri(char *s, void (*f) (unsigned int, char *))
 	i = 0;
 	if (s && f)
 	{
-		while (s[i])
-		{
-			f(i, &s[i]);
-			i++;
-		}
+		while (*s)
+			f(i++, &*(s++));
 	}
 }
